36026f0db15c9bd48e4b32bbe95b1666f276dc0b
michael.bourke@live.co.uk <michael.bourke@live.co.uk>
2021-05-04 02:11:01 +0100

    te

--- LiteDbRepo/Implementation/LiteDbRepo.cs
+++ LiteDbRepo/Implementation/LiteDbRepo.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using LiteDB;
 
-
 namespace DbLiteCore.Implementation
 {
     public class LiteDbRepo<EntityBaseModel> where EntityBaseModel : LiteDbEntity
